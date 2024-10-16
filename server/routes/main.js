aeb086a6514985084aad4ddc8612eb66e3a199b8
kaungsett <rrocket438@gmail.com>
2024-10-16 20:53:55 +0630

    add Layout

--- server/routes/main.js
+++ server/routes/main.js
@@ -6,9 +6,15 @@ const router = express.Router();
 
 
 router.get('',(req,res)=>{
-    res.render('index');
+    const locals ={
+        title : "NodeJs Blog",
+        description : " Simple BLog Created with NOdeJS,Express & Mongodb."
+    }
+    res.render('index',{locals});
 });
+
 router.get('/about',(req,res)=>{
     res.render('about');
 });
+
 module.exports = router;
