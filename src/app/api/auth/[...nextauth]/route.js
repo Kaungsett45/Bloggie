dd27b2186119b5ff745cb86874cd8442d78fac7d
kaungsett <rrocket438@gmail.com>
2025-01-29 10:42:11 +0630

    router & pj structure fix

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,19 +1,5 @@
 import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-
-export const authOptions = {
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    }),
-  ],
-  pages: {
-    signIn: '/signin', // Custom sign-in page URL
-    error: '/auth/error', // Custom error page
-  },
-  secret: process.env.NEXTAUTH_SECRET,
-};
+import { authOptions } from '@/lib/authOptions';
 
 const handler = NextAuth(authOptions);
-export { handler as GET, handler as POST };
+export { handler as GET, handler as POST }; 
